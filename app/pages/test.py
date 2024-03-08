c731568b01e2d0726953abd69174ca325ac5261d
寺田博英 <thakuei@th-MBP.local>
2024-03-08 14:05:05 +0900

    余分な空白

--- app/pages/test.py
+++ app/pages/test.py
@@ -5,7 +5,4 @@ def show_test_page():
     st.write("Welcome to after login page!")
     st.write('Welcome *%s*' % (st.session_state['name']))
     st.title('Some content')
-<<<<<<< Updated upstream
-=======
     
->>>>>>> Stashed changes
